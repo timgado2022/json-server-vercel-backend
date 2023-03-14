a511acb30e13593e2e5c52fbe34da22c4f56093c
team dev <timgado.project@gmail.com>
2023-03-14 22:41:23 +0100

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,7 +1,15 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+const fs = require("fs");
+const os = require("os");
+fs.copyFile("db.json", os.tmpdir() + "/db.json", function (err) {
+  if (err) console.log(err);
+  else console.log("copy file succeed to" + os.tmpdir());
+});
+
+
+const router = jsonServer.router(os.tmpdir() + '/db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
